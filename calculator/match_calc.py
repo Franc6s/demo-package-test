90cb9ea98226f8ab7ff470de91ab9d342e8535de
Francis Mangala <fdmangala@yahoo.com>
2025-03-22 20:58:12 -0400

    Updating repo

---
+++ calculator/match_calc.py
@@ -0,0 +1,24 @@
+class calculator :
+
+    def __init__(self):
+        pass
+
+    def add (self,a,b):
+        return a + b
+    
+    def subtract (self,a,b):
+        return a - b
+    
+    def divide(self,a,b):
+        try: 
+            return round((a/b),)
+        except ZeroDivisionError:
+            return "Error:Division by 0 is not allowed"
+
+    def multiply(self,a,b):
+        return a * b
+    
+calc = calculator()
+
+print(calc.multiply(6,3))
+ 
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='calc-demo',
+    version='1.0.0',
+    url='https://github.com/Franc6s/demo-package-test',
+    author='Francis Mangala',
+    author_email='francis@viz-optima.us',
+    description='Demo basic calculator',
+    license='MIT',
+    packages=['calculator']
+    )
